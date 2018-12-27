4b11057d39ae8ab064757afc3c43abe668bdbf6b
unknown <talvindersingh.ca@gmail.com>
2018-12-27 14:29:44 -0500

    added implementation for add to_shopping_list sing services and injectable

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,21 +1,49 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test recipe',
-         'https://upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg'),
+        new Recipe(
+            'Yummy Chicken', 
+            'This is Handi Chicken recipe',
+            '//upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg',
+            [
+                new Ingredient('Chicken', 100),
+                new Ingredient('Onions', 10),
+                new Ingredient('Tomato sauce', 10),
+                new Ingredient('Spices', 25)
+            ]),
 
-         new Recipe('Second Test Recipe', 'This is test recipe no. 2',
-         'https://upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg')
+         new Recipe(
+             'Tasty Spaghetti', 
+             'This is spaghetti recipe',
+             '//pixnio.com/free-images/2017/03/25/2017-03-25-09-34-43-725x483.jpg',
+             [
+                new Ingredient('Spaghetti', 125),
+                new Ingredient('Spices', 15),
+                new Ingredient('Wheat Flour', 25),
+                new Ingredient('Eggs', 3),
+                new Ingredient('Cheese', 2)
+            ])
       ];
 
+      constructor(private slService: ShoppingListService) {
+
+      }
 
       getRecipes() {
           // to use only the copy instead of the original.
           return this.recipes.slice(); 
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+          this.slService.addIngredients(ingredients);
+
       }
 
 }
