3b8d23a0d5ed8f59f6fcc01cb7dc7376bf5d049d
unknown <talvindersingh.ca@gmail.com>
2018-12-27 13:32:34 -0500

    coverted the Recipe part to use a service for events and communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { Recipe} from './recipe.model';
+import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test recipe',
          'https://upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg'),
