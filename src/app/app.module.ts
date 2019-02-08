85d9224bfbe67181b7c6ec04a785b927a359f0e3
unknown <talvindersingh.ca@gmail.com>
2019-02-08 13:07:06 -0500

    further division into some core and other modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
-//import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent,
-
-   ],
+  ],
    // Note: We must not declare components, directives and pipes in more than 1 module.
   imports: [
     BrowserModule, // contains the commonModules + other browser specific ones.Main app module should use this.
@@ -29,7 +22,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule    
+    AuthModule,
+    CoreModule    
   ],
   providers: [ShoppingListService, RecipeService], // having recipeservice here means we have 1-service that
   //  is handling all the components here viz: Recipe and ShoppingList, so that editing/adding to one
